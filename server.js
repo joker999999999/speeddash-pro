e0f76cf5ac752bafb288db40b65daf399163048f
SpeedDash Pro <i0991904@gmail.com>
2026-07-18 19:40:40 +0300

    Fix CSP for inline event handlers

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ app.use(helmet({
         directives: {
             defaultSrc: ["'self'"],
             scriptSrc: ["'self'", "'unsafe-inline'", 'https://cdnjs.cloudflare.com', 'https://js.stripe.com'],
+            scriptSrcAttr: ["'unsafe-inline'"],
             styleSrc: ["'self'", "'unsafe-inline'", 'https://cdnjs.cloudflare.com'],
             imgSrc: ["'self'", 'data:', 'blob:', 'https:'],
             fontSrc: ["'self'", 'data:', 'https://cdnjs.cloudflare.com'],
